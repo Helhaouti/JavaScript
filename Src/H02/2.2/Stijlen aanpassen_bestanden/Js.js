a2910c88b5a78048f3ec03c351fea4c803e1b7c9
Helhaouti <jye42GvDQCyDSAR>
2020-01-07 16:57:55 +0100

    Hoofdstuk 2

---
+++ Src/H02/2.2/Stijlen aanpassen_bestanden/Js.js
@@ -0,0 +1,14 @@
+document.getElementById("bl1").style.backgroundColor = "blue";
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.top = "10px" ;
+document.getElementById("bl2").style.textAlign = "center";
+
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.top = "40px" ;
+document.getElementById("bl3").style.textAlign = "right";
+
+
+document.getElementById("bl4").style.backgroundColor = "red";
+
